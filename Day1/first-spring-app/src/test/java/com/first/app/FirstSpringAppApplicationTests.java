06318887bc927c4fb882be574fd56c8cadd9e3fa
sfjbs13june <sfjbs13june@gmail.com>
2022-10-18 14:53:19 +0000

    updated the code day9

---
+++ Day1/first-spring-app/src/test/java/com/first/app/FirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
