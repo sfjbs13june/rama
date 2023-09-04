660031c46171f061eaeb8931613d4fc5dbfd943c
admin <rama@email.com>
2023-09-03 22:36:38 -0700

    first microservice

---
+++ day02/first-microservice/Readme.md
@@ -0,0 +1,9 @@
+## First microservice
+
+### Build microservice
+
+    mvn clean install
+
+### Run microservice
+
+    mvn spring-boot:run
---
+++ day02/first-web-microservice/src/main/java/com/rama/app/FirstWebMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rama.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebMicroserviceApplication.class, args);
+	}
+
+}
